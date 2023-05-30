6dfb577828280a2a17e0d4c5f8a330e1088b7b5d
Crowdbotics <engineering@crowdbotics.com>
2023-05-30 18:01:17 +0000

    Update store.

--- store/treidevAPI/api.js
+++ store/treidevAPI/api.js
@@ -18,12 +18,6 @@ function api_v1_vyui_create(payload) {
 function api_v1_login_create(payload) {
   return treidevAPI.post(`/api/v1/login/`, payload.data)
 }
-function api_v1_zak12_list(payload) {
-  return treidevAPI.get(`/api/v1/zak12/`)
-}
-function api_v1_zak12_create(payload) {
-  return treidevAPI.post(`/api/v1/zak12/`, payload.data)
-}
 function api_v1_signup_create(payload) {
   return treidevAPI.post(`/api/v1/signup/`, payload.data)
 }
@@ -84,18 +78,6 @@ function rest_auth_logout_retrieve(payload) {
 function rest_auth_logout_create(payload) {
   return treidevAPI.post(`/rest-auth/logout/`)
 }
-function api_v1_zak12_retrieve(payload) {
-  return treidevAPI.get(`/api/v1/zak12/${payload.id}/`)
-}
-function api_v1_zak12_update(payload) {
-  return treidevAPI.put(`/api/v1/zak12/${payload.id}/`, payload.data)
-}
-function api_v1_zak12_partial_update(payload) {
-  return treidevAPI.patch(`/api/v1/zak12/${payload.id}/`, payload.data)
-}
-function api_v1_zak12_destroy(payload) {
-  return treidevAPI.delete(`/api/v1/zak12/${payload.id}/`)
-}
 function api_v1_xewrwt_retrieve(payload) {
   return treidevAPI.get(`/api/v1/xewrwt/${payload.id}/`)
 }
@@ -141,8 +123,6 @@ export const apiService = {
   api_v1_vyui_list,
   api_v1_vyui_create,
   api_v1_login_create,
-  api_v1_zak12_list,
-  api_v1_zak12_create,
   api_v1_signup_create,
   api_v1_xewrwt_list,
   api_v1_xewrwt_create,
@@ -163,10 +143,6 @@ export const apiService = {
   api_v1_vyui_destroy,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
-  api_v1_zak12_retrieve,
-  api_v1_zak12_update,
-  api_v1_zak12_partial_update,
-  api_v1_zak12_destroy,
   api_v1_xewrwt_retrieve,
   api_v1_xewrwt_update,
   api_v1_xewrwt_partial_update,
