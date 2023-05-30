0478b819bca781ecb3a69ebee7edc90973651606
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-30 06:39:34 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bittest
+admin.site.register(Bittest)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Bittest(models.Model):
+    'Generated Model'
+    cittest = models.BigIntegerField()
+    mtest = models.BigIntegerField(null=True,blank=True,)
